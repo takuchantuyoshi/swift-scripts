2ab07e1f59dafe3f9eb225758897143ced71d5ff
takuchantuyoshi <da87435@gmail.com>
2019-05-01 21:16:34 +0900

    👍: スクリプトを修正

--- lib/Unused.rb
+++ lib/Unused.rb
@@ -52,6 +52,7 @@ class Item
 end
 
 module Unused
+  class unused
   def find
     items = []
     all_files = Dir.glob("**/*.swift").reject do |path|
@@ -184,7 +185,7 @@ module Unused
       !f.name.start_with?("test") && !f.modifiers.include?("@IBAction") && !f.modifiers.include?("override") && !f.modifiers.include?("@objc") && !f.modifiers.include?("@IBInspectable")
     }
   end
-
+ end
 end  
 
 class String
